7dddab2610372c23e0f644f3943e75f1ca426940
Jose Alejandro Mata <josealejandro.mata@cit.edu>
2026-03-11 16:07:24 +0800

    Update comment

--- BurnEffect.java
+++ BurnEffect.java
@@ -10,7 +10,7 @@ public class BurnEffect extends StatusEffect {
     public void applyStartOfTurn(Character c) { }
 
     @Override
-    public void applyEndOfTurn(Character c) {   // Deals damage at end of turn, then increases by 5
+    public void applyEndOfTurn(Character c) {   // Deals damage at end of turn
         int dmg = magnitude;
         System.out.println(c.name + " is burned, losing " + dmg + " HP.");
         c.takeDamage(dmg);
